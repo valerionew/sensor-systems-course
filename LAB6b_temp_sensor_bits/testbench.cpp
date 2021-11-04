22d29b74b1cf9eb0d0e2e52180bd2a06b7c09b8a
valerionew <valerionappi995@gmail.com>
2021-11-04 15:51:25 +0100

    added test bench for conversion of 11 bit samples

---
+++ LAB6b_temp_sensor_bits/testbench.cpp
@@ -0,0 +1,18 @@
+// test at https://ideone.com/yXCnWu
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	float should_be[5] = {	0, -0.125,-25.0, -54.875, -55.000};   
+	uint8_t temperature_bytes[2*5] = {0,0,0xFF,0xE0,0xE7,0x0,0xC9,0xA0,0xC9,0x0};
+	
+	for(int i = 0; i<5; i++)
+	{
+		float ftemp = ((int16_t) ((uint16_t)temperature_bytes[2*i] << 8 | temperature_bytes[2*i+1]))/256.0;
+		// float ftemp = temp / 256.0;
+		cout << ftemp << ' ' << should_be[i] <<endl;
+	}
+
+	return 0;
+}
