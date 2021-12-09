f46d08ff4fab23f2d1c623791aac69315bb6b996
valerionew <valerionappi995@gmail.com>
2021-12-09 13:20:08 +0100

    faster matrix multiplex for better visual fx

--- LAB8b_letter_dma/Core/Src/main.c
+++ LAB8b_letter_dma/Core/Src/main.c
@@ -249,7 +249,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 4000-1;
+  htim1.Init.Prescaler = 1000-1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 96-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
